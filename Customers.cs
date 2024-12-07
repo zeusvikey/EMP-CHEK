245d3f48fed81f9010a7793575a5a8f7710779df
zeusvikey <127013256+zeusvikey@users.noreply.github.com>
2024-12-07 05:42:13 +0530

    GET SET FUNSTION

---
+++ Customers.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopsProject1
+{
+   
+    public class Customers
+    {
+        int _Custid;
+        bool _Status;
+        string _Name, _State, _Country;
+        double _Balenced;
+        cities _City;
+        public Customers(int Custid) {
+            Console.WriteLine("!!!!!!!!!!!WELCOME TO THE CUSTM BANK!!!!!!!!!!!\n");
+            int _Custid = Custid;
+            _Status = false;
+            Console.WriteLine("Enter your Name");
+            _Name = Console.ReadLine();
+            Console.WriteLine("Enter your Balenced");
+            _Balenced = double.Parse(Console.ReadLine());
+            _City = cities.Bangalore;
+            _State = "Karnataka";
+            _Country = "india";
+             }
+      
+        public int Custid {
+            get { return _Custid; }
+            
+        }
+        public bool Status
+        {
+            get { return _Status; }
+            set { _Status = value; }
+        }
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                if (_Status)
+                {
+                    _Name = value;
+                }
+            }
+        }
+        public double Balenced
+        {
+            get
+            {
+                if(_Status)
+                {
+                    return _Balenced;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            set
+            {
+                if (_Status)
+                {
+                    if (_Balenced > 500)
+                    {
+                        _Balenced = value;
+                    }
+                }
+            }
+        }
+        
+        public string State
+        {
+            get { return _State; }
+
+            protected set
+            {
+                if(_Status)
+                {
+                    _State = value;
+                }
+            }
+        }
+        public cities City
+        {
+            get { return _City; }
+            set
+            {
+                if (_Status)
+                {
+                    _City = value;
+                }
+            }
+        }
+        public string Country
+        {
+            get;
+            private set;
+        }
+        public string Continent { get; } = "Asia";
+    
+
+
+
+}
+}
---
+++ TestCustomers.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopsProject1
+{
+    internal class TestCustomers
+    {
+        static void Main(string[] args)
+        
+        {
+            Customers obj = new Customers(1000);
+            Console.WriteLine("Custid:" + obj.Custid);
+
+            //if (obj.Status)
+            //{
+            //    Console.WriteLine("Customers Staus : Active");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Customer Status: In-Active");
+            //    Console.WriteLine("Customer Name: " + obj.Name + "\n ");
+            //}
+            obj.Status = true;
+            if (obj.Status)
+                Console.WriteLine("Customer Status: Active");
+            else
+                Console.WriteLine("Customer Status: In-Active\n");
+            Console.WriteLine("Customer Name: " + obj.Name + "\n ");
+
+            //HERE CONDINTION FOR UPDATE:=>>
+            Console.WriteLine("IF YOU WANT TO UPDATE THEN PRESS Y AND ENTER OR PRESS N FOR NOTHING UPDATED:");
+            string input = Console.ReadLine();
+            if (input.Equals("Y", StringComparison.OrdinalIgnoreCase))
+            {
+                if (obj.Status)
+                {
+                    Console.WriteLine("Enter the value to update the Name:");
+                    string a = Console.ReadLine();
+                    obj.Name += a;
+
+                    // Console.WriteLine("Name when update succeeded: " + obj.Name + "\n");
+                    //Console.WriteLine("Balance when status is active: " + obj.Balenced + "\n"); 
+                }
+                else
+                {
+                    Console.WriteLine("Status is inactive. Cannot update.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nothing updated.");
+                Console.WriteLine("Customer Status: In-Active");
+                Console.WriteLine("Customer Name: " + obj.Name);
+                Console.WriteLine("Name when update succeded: " + obj.Name);
+                Console.WriteLine("Balance when status is active: " + obj.Balenced + "\n");
+                return;
+            }
+
+
+
+
+            if (obj.Balenced <= 4600)
+            { //Transaction failed
+                Console.WriteLine("Balance  transaction failed: " + obj.Balenced);
+            }
+            else if (obj.Balenced >= 4500)
+            {
+                Console.WriteLine("Balance  transaction succeded: " + obj.Balenced + "\n");
+            }
+               
+            obj.Status = true; //Activating the status
+            if (obj.Status)
+                Console.WriteLine("Customer Status: Active");
+            else
+                Console.WriteLine("Customer Status: In-Active"+ "\n");
+            Console.WriteLine("Customer Name: " + obj.Name);
+            Console.WriteLine("Name when update succeded: " + obj.Name);
+            Console.WriteLine("Balance when status is active: " + obj.Balenced);
+            Console.WriteLine("State Name: " + obj.State);
+            Console.WriteLine("City Name: " + obj.City);
+            Console.WriteLine("Country Name: " + obj.Country);
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
+//Console.WriteLine("IF YOU WANT TO UPDATE THEN UPDATE then PRESS ENTER Y:");
+//if (obj.Status = y)
+//{
+//    if (obj.Status)
+//    {
+
+//        string a = Console.ReadLine();
+//        obj.Name += a;
+
+//        Console.WriteLine("Name when update succeeded: " + obj.Name + "\n");
+//        Console.WriteLine("Balance when status is active: " + obj.Balenced + "\n");
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Nothing update");
+//    return;
+//}  write the clean code 
